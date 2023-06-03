20a4dc2a9f6208f041fbe274495dcc0aa0e9005f
GlindaRose <45208607+GlindaRose@users.noreply.github.com>
2023-06-03 13:40:48 +0100

    deleted redundant js form validator code - will sort some other way

--- js/myscripts.js
+++ js/myscripts.js
@@ -6,10 +6,3 @@ function openTab(tabName) {
     }
     document.getElementById(tabName).style.display = "block"; 
 }
-
-//Form Validator
-var frmvalidator  = new Validator("contact-form");
-frmvalidator.addValidation("name","req","Please provide your name");
-frmvalidator.addValidation("email","req","Please provide your email");
-frmvalidator.addValidation("email","email",
-  "Please enter a valid email address");
